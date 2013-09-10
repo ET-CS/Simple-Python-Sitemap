5c45fa9e93f397b6588cc51583466282b1306645
ET-CS <et.programming@gmail.com>
2013-09-10 05:44:16 +0300

    small css fixes

--- index.py
+++ index.py
@@ -13,9 +13,9 @@ def index(req):
     path = os.path.dirname(os.path.abspath(__file__))
     engine = tenjin.Engine(path=[path+'/views'], layout='_layout.pyhtml')
     for key in settings.context['items']:
-        thumbnail = key[0]+'/thumbnail.png'
-        filename = path+'/thumbs/'+key[0].split('//')[1]+'.png'
-        if os.path.isfile(filename):
+        filename = key[0].split('//')[1]+'.png'
+        thumbnail = 'thumbs/'+filename
+        if os.path.isfile(path+'/thumbs/'+filename):
             key.append(thumbnail)
         else:
             key.append('')
